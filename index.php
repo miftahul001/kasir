33388c0b11b5e165e14dd9e7b3051c44f054be80
miftahul001 <miftahulmunir001@gmail.com>
2025-09-22 06:54:13 +0700

    add dropdown navigation

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Test</title>
-<link href="favicon.svg" rel="icon" sizes="any" type="image/svg+xml">
+<title>Kasir</title>
+<link href="img/logo.svg" rel="icon" sizes="any" type="image/svg+xml">
 
 <style>
 * {
 	box-sizing: border-box;
-	font-size: 1.5vmin;
+	font-family: Tahoma, Verdana, sans-serif;
 }
 body {
 	margin: 0;
